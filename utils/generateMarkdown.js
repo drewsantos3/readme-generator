36464d6ab883c5026b7aa3f07a86659fcff01122
Drew S <andrewsantos2022@u.northwestern.edu>
2023-03-03 18:24:05 -0600

    finished index and generatemkdn.js

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,60 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  } else {
+  return "";
+}
+};
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `* [License](#license)`;
+  } else {
+  return "";
+  }
+};
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+  # Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribution)
+  * [Test](#test)
+  * [License](#license)
+  * [Questions](#questions)
+  
+  # Description
+  ${data.description}
+
+  # Installation
+  ${data.installation}
+  
+  # Usage
+  ${data.usage}
+
+  # Contribution
+  ${data.contribution}
+
+  # Test
+  ${data.test}
+
+  # License
+  ${data.license}
+
+  # Questions
+  If you have any questions, please contact me at ${data.email} or visit my GitHub page at [${data.github}](https:github.com/${data.github}).
+`;
+};
+
+module.exports = generateMarkdown;
